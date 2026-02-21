090c05967cd913b7ae5d3ceb5c55d913014950cf
Amna <amina.ali.31032008@gmail.com>
2026-02-21 11:18:17 +0500

    Added product page

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const db = require('./config/db');
+const path = require('path');
 
 const app = express();
 
+app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Test route
@@ -11,16 +16,37 @@ app.get('/', (req, res) => {
   res.send("Quotation App is Running");
 });
 
-// Example DB test route
-app.get('/test-db', (req, res) => {
-  db.query('SELECT 1 + 1 AS result', (err, results) => {
-    if (err) {
-      return res.status(500).send(err);
+app.get('/admin/add-product', (req,res) => {
+  db.query("SELECT * FROM categories", (err,results) => {
+    if (err){
+      console.log(err);
+      return res.render('add_product', {categories:''});
     }
-    res.json(results);
+    // Else send data
+    return res.render('add_product', { categories: results})
   });
+  
 });
+app.post('/admin/add-product', (req, res) => {
 
+  const { serial, name, cPrice, cost, category } = req.body;
+
+  db.query(
+    `INSERT INTO products 
+     (serial_no, description, price, cost, category_id)
+     VALUES (?, ?, ?, ?, ?)`,
+    [serial, name, cPrice, cost, category],
+    (err) => {
+      if (err) {
+        console.log(err);
+        return res.redirect('/admin/add-product');
+      }
+
+      return res.redirect('/admin/add-product');
+    }
+  );
+
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
